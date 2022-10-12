8b8ba7945a3a868bd37cf078598352d4525e2ace
nkjcolin <49198636+nkjcolin@users.noreply.github.com>
2022-10-12 17:22:00 +0800

    Add files via upload

--- fade.c
+++ fade.c
@@ -1,12 +1,20 @@
 #include <wiringPi.h>
 #include <stdio.h>
 #include <softPwm.h>
+#include <stdbool.h>
+#include <string.h>
+#include <termio.h>
+#include <unistd.h>
 
 #define RED 24
 #define GREEN 23
 
 
 void askMode();
+void blink();
+void turnON();
+void turnOFF();
+bool kbhit(); //detect keypress
 
 
 int choice = 0;
@@ -27,30 +35,57 @@ int main()
     {
       switch(choice)
       {
-       case 0:
 
-        digitalWrite(RED, LOW);
-        digitalWrite(GREEN, LOW);
-
+        case 0:
+          turnOFF();
         break;
 
         case 1:
-
-        digitalWrite(RED, HIGH);
-        digitalWrite(GREEN, HIGH);
-
+          turnON();
         break;
+
+        case 2:
+          while(choice == 2)
+          {
+            blink();
+            if(kbhit())
+            {
+              int k = getchar();
+              if(k == '0')
+              {
+                turnOFF();
+                break;
+              }
+              if(k == '1')
+              {
+                turnON();
+                break;
+              }
+              if(k == '2')
+              {
+                blink();
+              }
+            }
+          }
+        break;
+
+        default:
+        break;
+
+
       }
 
     }
-    else
+
+    else if(choice != 0 || choice != 1 || choice != 2 || choice != 3)
     {
-      askMode();
+      printf("\nINVALID\n");
+      break;
     }
 
   }    
 
-
+return 0;
 
 }
 
@@ -58,11 +93,43 @@ int main()
 void askMode()
 {
   printf("\nEnter mode 0, 1, 2, 3: ");
-  scanf("%d", &choice);
+  scanf("\n%d", &choice);
 
 }
 
+void blink()
+{
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  delay(500);
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  delay(500); 
+}
 
+void turnON()
+{
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+}
 
+void turnOFF()
+{
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+}
 
-
+bool kbhit()
+{
+    struct termios original;
+    tcgetattr(STDIN_FILENO, &original);
+    struct termios term;
+    memcpy(&term, &original, sizeof(term));
+    term.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    int characters_buffered = 0;
+    ioctl(STDIN_FILENO, FIONREAD, &characters_buffered);
+    tcsetattr(STDIN_FILENO, TCSANOW, &original);
+    bool pressed = (characters_buffered != 0);
+    return pressed;
+}
