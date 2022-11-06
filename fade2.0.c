741ea5b154b459eacecfacb7b94706530d2f51d1
nkjcolin <49198636+nkjcolin@users.noreply.github.com>
2022-11-07 00:33:24 +0800

    updated gpio pin 13 19

--- fade2.0.c
+++ fade2.0.c
@@ -7,8 +7,8 @@
 #include <termio.h>
 #include <unistd.h>
 
-#define RED 24    // GPIO 24 (Pin 16)
-#define GREEN 23  // GPIO 23 (Pin 18)
+#define RED 13    // GPIO 24 (Pin 16)
+#define GREEN 19  // GPIO 23 (Pin 18)
 
 void askMode();
 void blink2Hz();
